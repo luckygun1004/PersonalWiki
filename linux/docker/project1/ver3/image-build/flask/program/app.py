16879f5811bc3c97ba70d063e1b8e322988e88de
root <root@host-10-0-3-215.kr-central-2.c.internal>
2024-11-12 11:56:44 +0900

    ver3 update

---
+++ linux/docker/project1/ver3/image-build/flask/program/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+import logging
+
+logging.basicConfig(filename = "log/project.log", level = logging.DEBUG)
+server = Flask(__name__)
+
+
+@server.route('/')
+def hello_world():
+    return 'hello elasticsearch2'
